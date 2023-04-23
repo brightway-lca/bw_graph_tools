b6dce3522f90b4ce0e93d818bf523b2641de2fb2
Chris Mutel <cmutel@gmail.com>
2023-04-23 14:51:42 +0200

    Add new `GraphTraversal` class
    No assumptions on reference product exchanges, and good use of `dataclasses`.

--- bw_graph_tools/__init__.py
+++ bw_graph_tools/__init__.py
@@ -1,19 +1,19 @@
 __all__ = (
     "__version__",
-    "to_normalized_adjacency_matrix",
+    "AssumedDiagonalGraphTraversal",
+    "Edge",
+    "Flow",
     "get_path_from_matrix",
-    "path_as_brightway_objects",
     "GraphTraversal",
+    "Node",
+    "path_as_brightway_objects",
+    "to_normalized_adjacency_matrix",
 )
 
 from .graph_traversal_utils import get_path_from_matrix, path_as_brightway_objects
 from .matrix_utils import to_normalized_adjacency_matrix
 from .utils import get_version_tuple
-
-try:
-    from .graph_traversal import GraphTraversal
-except ImportError:
-    print("Graph traversal class not imported; it relies on `bw2calc` library.")
+from .graph_traversal import GraphTraversal, Node, Edge, Flow, AssumedDiagonalGraphTraversal
 
 
 __version__ = get_version_tuple()
